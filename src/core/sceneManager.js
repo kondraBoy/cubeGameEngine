f1cc27a5f7f37250d3e8171b6e3565be00e60383
guarinoandreamio@gmail.com <ANDREA.GUARINO>
2025-12-04 11:31:55 +0100

    #002 camera fissa su player + wasd ok

--- src/core/sceneManager.js
+++ src/core/sceneManager.js
@@ -8,13 +8,21 @@ export class SceneManager {
 
     createScene() {
         const scene = new BABYLON.Scene(this.engine); //scena generale
-        const camera = new BABYLON.ArcRotateCamera( //camera DA FIXARE E METTERE IN 3 PERSONA
-            "camera",
-            Math.PI / 2, Math.PI / 3,
-            10,
-            new BABYLON.Vector3(0,1,0),
+        const camera = new BABYLON.FollowCamera(
+        "FollowCam",
+            // Posizione iniziale della camera
+            new BABYLON.Vector3(0, 2, -6),
             scene
         );
+        // oggetto da seguire
+        camera.lockedTarget = this.player;
+
+        // Parametri principali della FollowCamera
+        camera.radius = 6;               // distanza dal player
+        camera.heightOffset = 2;         // altezza rispetto al player
+        camera.rotationOffset = 180;     // da che angolo guardare il player
+        camera.cameraAcceleration = 0.05; // velocità con cui la camera “insegue”
+        camera.maxCameraSpeed = 20;       // limite massimo della velocità
         camera.attachControl(this.game.canvas, true); //attacco della camera
         new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene); //settings luce
         this.player = BABYLON.MeshBuilder.CreateBox("player", {}, scene); //creazione del player

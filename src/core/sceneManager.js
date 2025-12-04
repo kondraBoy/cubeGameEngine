b65f5b690d842173530cb9788b4133f3f8ee03b6
kondraBoy <guarinoandreamio@gmail.com>
2025-12-04 09:48:23 +0100

    #001 to fix camera

--- src/core/sceneManager.js
+++ src/core/sceneManager.js
@@ -3,14 +3,21 @@ export class SceneManager {
     constructor(engine, game) {
         this.engine = engine;
         this.game = game;
+        this.player = null;
     }
 
     createScene() {
-        const scene = new BABYLON.Scene(this.engine); //scena
-        const camera = new BABYLON.ArcRotateCamera("camera", Math.PI / 2, Math.PI / 3, 10, BABYLON.Vector3(0,1,0), scene); //camera
-        camera.attachControl(this.game.canvas, true); //controlli camera
-        const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene); //luce
-        BABYLON.MeshBuilder.CreateBox("player", {}, scene); //player
+        const scene = new BABYLON.Scene(this.engine); //scena generale
+        const camera = new BABYLON.ArcRotateCamera( //camera DA FIXARE E METTERE IN 3 PERSONA
+            "camera",
+            Math.PI / 2, Math.PI / 3,
+            10,
+            new BABYLON.Vector3(0,1,0),
+            scene
+        );
+        camera.attachControl(this.game.canvas, true); //attacco della camera
+        new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene); //settings luce
+        this.player = BABYLON.MeshBuilder.CreateBox("player", {}, scene); //creazione del player
         return scene;
     }
 }
